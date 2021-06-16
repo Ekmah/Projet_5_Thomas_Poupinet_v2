0e2f8df3bd890dcb29b2b67d71ef05c0afd0a74c
Thomas Poupinet <th.poupinet@free.fr>
2021-06-16 08:47:05 +0200

    get GET infos

--- frontend/js/order.js
+++ frontend/js/order.js
@@ -5,6 +5,6 @@ const email = params.get("email");
 const firstName = params.get("firstname");
 const lastName = params.get("lastname");
 
-var form = {"adress":adress}
+var form = {adress:adress, city:city, email:email, firstNzme:firstName, lastName:lastName}
 console.log(JSON.stringify(form))
 products = Storage.get("products")
